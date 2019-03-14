941cae493c394318cb8fc8df02685561bd4dc4df
Mendel Greenberg <chabad360@gmail.com>
2019-03-14 00:54:38 -0400

    adding logging and fixing MORE typos...

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,9 @@ if [[ ! -d "${BENCH}/sites" ]]; then
     dockerize -template /home/frappe/templates/procfile.tmpl:${BENCH}/Procfile -template /home/frappe/templates/common_site_config.tmpl:${BENCH}/sites/common_site_config.json
 fi
 
+cat ${BENCH}/Procfile
+cat ${BENCH}/sites/common_site_config.json
+
 cd "${BENCH}" || exit 1
 su-exec frappe bench set-mariadb-host "${MARIADB_HOST}"
 
