0e98192fb658420015c94e9b3a7f8e6e0934f131
Mendel Greenberg <chabad360@gmail.com>
2019-04-07 02:48:27 -0400

    Just cause for whatever unknown reason this might work

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 while ! [[ $i == 20 ]]
 do
-    output=$( curl "http://localhost:8000" )
+    output=$( curl "http://127.0.0.1:8000" )
     { echo "Exit status of curl: $?"
     } 1>&2
     sleep 2
