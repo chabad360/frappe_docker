039e4e978942c70bc552d452d5afdb504322c9de
Mendel Greenberg <chabad360@gmail.com>
2019-04-09 02:26:54 -0400

    a few more small changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,9 @@ chown -R frappe "${BENCH}"
 # Setup bench
 if [[ ! -d "${BENCH}/sites" ]]; then
     su-exec frappe bench init "${BENCH}" --ignore-exist --skip-redis-config-generation --verbose
-    dockerize -template /home/frappe/templates/procfile.tmpl:${BENCH}/Procfile -template /home/frappe/templates/common_site_config.tmpl:${BENCH}/sites/common_site_config.json
 fi
+
+dockerize -template /home/frappe/templates/procfile.tmpl:${BENCH}/Procfile -template /home/frappe/templates/common_site_config.tmpl:${BENCH}/sites/common_site_config.json
 
 cd "${BENCH}" || exit 1
 su-exec frappe bench set-mariadb-host "${MARIADB_HOST}"
