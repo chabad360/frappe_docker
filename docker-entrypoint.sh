ea8d1acc05b7d959cc43e9092b4a29b1b2bb8684
chabad360 <chabad360@gmail.com>
2019-11-15 05:24:37 +0000

    and that?

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,9 +9,9 @@ if [[ ! -d "${BENCH}/sites" ]]; then
 cd ${BENCH}
 su-exec frappe mkdir -p apps logs commands
 cd apps 
-su-exec frappe pip3 install --no-cache-dir testfm
+su-exec frappe PATH="${PATH}:/home/frappe/.local/bin" pip3 install --no-cache-dir testfm
 su-exec frappe git clone --depth 1 -o upstream https://github.com/frappe/frappe 
-su-exec frappe pip3 install --no-cache-dir -e ${BENCH}/apps/frappe \
+su-exec frappe PATH="${PATH}:/home/frappe/.local/bin" pip3 install --no-cache-dir -e ${BENCH}/apps/frappe \
 cd frappe 
 su-exec frappe yarn
 su-exec frappe yarn run production 
